9db71a9908db86bf030bd95c865ee3e665ff1f93
schrammlb2 <schrammlb2@gmail.com>
2024-04-08 18:37:09 -0400

    changed run script

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,4 @@
-for task in "hopper-medium-v2" "hopper-medium-expert-v2";
+for task in "hopper-medium-v2";# "hopper-medium-expert-v2";
 do 
 	echo $task
 	for i in {1..5};
@@ -6,8 +6,8 @@ do
 		echo $i
 		python run_example/run_som_reg_only.py --task=$task --epoch=250 --seed=$i
 		python run_example/run_som_regularized_sac.py --task=$task --epoch=250 --seed=$i
-		# python run_example/run_cql.py --task=$task --epoch=250 --seed=$i
-		# python run_example/run_td3bc.py --task=$task --epoch=250 --seed=$i
-		# python run_example/run_iql.py --task=$task --epoch=250 --seed=$i
+		python run_example/run_cql.py --task=$task --epoch=250 --seed=$i
+		python run_example/run_td3bc.py --task=$task --epoch=250 --seed=$i
+		python run_example/run_iql.py --task=$task --epoch=250 --seed=$i
 	done
 done
